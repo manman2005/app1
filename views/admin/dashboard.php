7f4b4034bf7074eed95b712b73cf87cc30765801
mamman2005 <sagase66@gmail.com>
2026-01-15 12:11:31 +0700

    ลบเนปบาร์ แก้ไขไฟล์ลิงค์หน้าเนปบาร์

--- views/admin/dashboard.php
+++ views/admin/dashboard.php
@@ -1,5 +1,5 @@
 <?php include_once __DIR__ . '/../partials/header.php'; ?>
-<?php include_once __DIR__ . '/../partials/admin_navbar.php'; ?>
+
 
 <div class="container mx-auto px-6 py-8">
     <h1 class="text-4xl font-bold text-text-main mb-8 text-center">Admin Dashboard</h1>
--- views/admin/edit_user.php
+++ views/admin/edit_user.php
@@ -1,5 +1,5 @@
 <?php include __DIR__ . '/../partials/header.php'; ?>
-<?php include __DIR__ . '/../partials/admin_navbar.php'; ?>
+
 
 <div class="container mx-auto p-4">
     <h1 class="text-2xl font-bold mb-4">Edit User</h1>
